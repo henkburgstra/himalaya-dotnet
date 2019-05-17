9eecd80f7d3c39273e3899eb07cdec469862fdeb
Henk Burgstra <henk.burgstra@gmail.com>
2019-05-17 11:22:19 +0200

    Added unit test

---
+++ HimalayaDotnet/HimalayaDotnet/Test/HimalayaTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HimalayaDotnet.Test
+{
+    public class HimalayaTests
+    {
+        private string _testDoc = @"[{
+              type: 'element',
+              tagName: 'div',
+              attributes: [{
+                key: 'class',
+                value: 'post post-featured'
+              }],
+              children: [{
+                type: 'element',
+                tagName: 'p',
+                attributes: [],
+                children: [{
+                  type: 'text',
+                  content: 'Himalaya parsed me...'
+                }]
+              }, {
+                type: 'comment',
+                content: ' ...and I liked it. '
+              }]
+            }]";
+
+        [Fact]
+        public void ParseJsonTest()
+        {
+            var elements = HimalayaElement.ParseJson(_testDoc);
+            Assert.Single(elements);
+            var element = elements[0];
+            Assert.Equal("element", element.Type);
+            Assert.Equal("div", element.TagName);
+            Assert.Single(element.Attributes);
+            var attr = element.Attributes[0];
+            Assert.Equal("class", attr.Key);
+            Assert.Equal(2, element.Children.Count);
+            var child = element.Children[0];
+            Assert.Equal("element", child.Type);
+            Assert.Equal("p", child.TagName);
+        }
+    }
+}
