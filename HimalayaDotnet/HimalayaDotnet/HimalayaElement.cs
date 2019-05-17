38d2df0b77760ed0f42cdc648d58340b032f0112
Henk Burgstra <henk.burgstra@gmail.com>
2019-05-17 10:08:26 +0200

    Added classes HimalayaElement and HimalayaAttribute

---
+++ HimalayaDotnet/HimalayaDotnet/HimalayaElement.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HimalayaDotnet
+{
+    public class HimalayaElement
+    {
+        public string Type { get; set; } = "";
+        public string TagName { get; set; } = "";
+        public List<HimalayaAttribute> Attributes { get; set; } = new List<HimalayaAttribute>();
+        public List<HimalayaElement> Children { get; set; } = new List<HimalayaElement>();
+
+        public static List<HimalayaElement> ParseJson(string json)
+        {
+            return JsonConvert.DeserializeObject<List<HimalayaElement>>(json);
+        }
+    }
+}
